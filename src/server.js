a722089c2c3ebd7971b2ededf85c46092e9afa17
Supreet <me.supi@gmail.com>
2020-04-19 19:33:21 -0400

    Created server file

--- src/server.js
+++ src/server.js
@@ -1,7 +1,12 @@
-const app = require("./app");
+const express = require('express');
+ const app = express();
 
-const { PORT } = require("./config");
+ const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => {
-    console.log(`Server listening at http://localhost:${PORT}`);
-});
+ app.get('/api/*', (req, res) => {
+   res.json({ok: true});
+ });
+
+ app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
+
+ module.exports = {app};
